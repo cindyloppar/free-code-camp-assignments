862dfa5921bcf79d191342bcef0f0505ebd1a5c8
cindyloppar <cindyloppar@gmail.com>
2017-11-13 15:13:29 +0200

    I added an if statement to compare the the boolean primitives

--- boo-whoo.js
+++ boo-whoo.js
@@ -1,5 +1,6 @@
 function booWho(bool){
-    return typeof bool === 'boolean'
+    return typeof bool === 'boolean';
+    // return (str === true || str === false);
     // if(str === true || str === false){
     //     return true;
     // }else{
