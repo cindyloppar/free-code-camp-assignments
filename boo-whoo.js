ab7ab1dd3a8b18925724af9d9ecbc8a31019c210
cindyloppar <cindyloppar@gmail.com>
2017-11-15 12:46:43 +0200

    I edited the first file and added an if-statement and the other file I added two for-loops and one if statement

--- boo-whoo.js
+++ boo-whoo.js
@@ -1,10 +1,11 @@
-function booWho(bool){
-    return typeof bool === 'boolean';
+function booWho(bool) {
+    // return typeof bool === 'boolean';
     // return (str === true || str === false);
-    // if(str === true || str === false){
-    //     return true;
-    // }else{
-    //     return false
-    // }
+
+    if (str === true || str === false) {
+        return true;
+    } else {
+        return false
+    }
 }
 console.log(booWho(null))
---
+++ sorted-union.js
@@ -0,0 +1,15 @@
+function sortNumbers(arr1, arr2, arr3) {
+    var newArray = [];
+    for (var i = 0; i < arguments.length; i++) {
+        var array = arguments[i];
+    }
+    for (var K = 0; K < arguments.length; K++) {
+        var arr = array[j]
+    }
+    if (arr.indexOf(arr) < 0) {
+newArray.push(arr);
+    }
+    return newArray;
+}
+
+console.log(sortNumbers[1, 3, 2], [5, 2, 1, 4], [2, 1]);
