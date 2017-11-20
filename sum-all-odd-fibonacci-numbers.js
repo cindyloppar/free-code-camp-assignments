5ffca85e6c958c4bba0e912ba3dcd85a93382244
cindyloppar <cindyloppar@gmail.com>
2017-11-20 16:21:02 +0200

    I added a while loop and an if-statement

--- sum-all-odd-fibonacci-numbers.js
+++ sum-all-odd-fibonacci-numbers.js
@@ -8,7 +8,14 @@ function sumFibs(arr) {
     var prevNumber = 0;
     var currNumber = 1;
     var result = 0;
-    
+    while (currNumber <= num) {
+        if (currNumber % 2 !== 0) {
+            result += currNumber;
+        }
+
+        currNumber += prevNumber;
+        prevNumber = currNumber - prevNumber;
+    }
 
     return result;
 }
