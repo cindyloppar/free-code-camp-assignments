2c67922b18228b00de17ae191617ffeddfc229f7
cindyloppar <cindyloppar@gmail.com>
2017-11-20 09:46:26 +0200

    I add regex and replaced spaces with dashes

--- spinal-tap-case.js
+++ spinal-tap-case.js
@@ -1,7 +1,8 @@
 //Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
 function spinalCase(str) {
-  
+  var string = /\s+|_+/g;
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2').replace(string, '-').toLowerCase();
   return str;
 }
 
