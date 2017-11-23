a284626bf994f765ba6a6b0abb7c35833754a6dc
cindyloppar <cindyloppar@gmail.com>
2017-11-23 11:10:28 +0200

    I added a for-loop and if-statement

--- drop-it.js
+++ drop-it.js
@@ -1,4 +1,11 @@
 function dropElements(arr, func){
+    for(var i = 0; i < arr.length; i++){
+        if(func(arr[0])){
+
+        }else{
+            arr.shift();
+        }
+    }
     return arr;
 
 }
