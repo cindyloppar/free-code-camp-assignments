e6651c33a93bd12655bbd29be0d94d1e103c9662
cindyloppar <cindyloppar@gmail.com>
2017-11-29 15:43:59 +0200

    I added a function and a for-loop

--- free-code-camp-assignment/arguments-optional.js
+++ free-code-camp-assignment/arguments-optional.js
@@ -1,16 +1,23 @@
 
 function sumTwoArguments(firstArgument,secondArgument){
-
-    // returns an object {'0':2, '1':3}.
-    // return arguments; 
-
-// returns an array of the arguments.
-  var arr = [];
-  for(var i = 0; i < arguments.length; i++){
-      arr.push(arguments[i]);
-    }
-    return arr;
+   var args = Array.from(arguments);
+    var sum = 0;
+    if (secondArgument) {
+        var answer = firstArgument + secondArgument;
+        if (typeof answer !== "number") {
+            return undefined;
+        } else {
+            return answer;
+        }
+    } else
+        if (typeof firstArgument !== "number") {
+            return undefined;
+        }
+    return function (secondArgument) {
+        var answer = firstArgument + secondArgument;
+        if (typeof answer == "string") { return undefined; }
+        else { return answer; }
+    };
 }
-
-
+    
 console.log(sumTwoArguments(2,3));
--- free-code-camp-assignment/telephone-checker.js
+++ free-code-camp-assignment/telephone-checker.js
@@ -1,3 +1,7 @@
-function telephoneChecker(){
-
+function telephoneCheck(str){
+    var reg = /^\d{10}$/|/^\d{3}[-]\d{4}$/
+return true;
 }
+
+console.log(telephoneCheck("5555555555"))
+console.log(telephoneCheck("555-5555")); 
