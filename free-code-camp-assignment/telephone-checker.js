581474ba287ed7351d96b373d276ecee7585701a
cindyloppar <cindyloppar@gmail.com>
2017-12-01 15:48:05 +0200

    I added two regax but it works for only two and no longer works for the first one

--- free-code-camp-assignment/telephone-checker.js
+++ free-code-camp-assignment/telephone-checker.js
@@ -1,9 +1,10 @@
 function telephoneCheck(str){
-    var reg =  /^(1|1 )?(\d{3}|\(\d{3}\))[-\s]?\d{3}[-\s]?\d{4}$/;
-    // /^\d{10}$/|/^\d{3}[-]\d{4}$/|/^\d{3}[-]\d{4}$/
-return true;
+    var reg =    /^(1|1 )?(\d{3}|\(\d{3}\))[-\s]/;
+return reg.test(str);
 }
 
-console.log(telephoneCheck("5555555555"))
-console.log(telephoneCheck("555-5555")); 
+console.log(telephoneCheck("5555555555")) 
 console.log(telephoneCheck("(555)555-5555"));
+console.log(telephoneCheck("1 555 555 5555"));
+console.log(("555 555 5555")); 
+
