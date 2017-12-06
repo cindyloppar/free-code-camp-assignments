e2f7a74ab2a270618fa6e7f663aa62c07d587fd1
cindyloppar <cindyloppar@gmail.com>
2017-12-06 09:56:39 +0200

    I added an artist for the recording collection which didn't have one

--- free-code-camp-assignment/telephone-checker.js
+++ free-code-camp-assignment/telephone-checker.js
@@ -1,5 +1,5 @@
 function telephoneCheck(str){
-    var reg =    /^(1|1 )?(\d{3}|\(\d{3}\))[-\s]?\d{3}[-\s]?\d{4}$/;
+    var reg = /^(1|1 )?(\d{3}|\(\d{3}\))[-\s]?\d{3}[-\s]?\d{4}$/;
 return reg.test(str);
 }
 
