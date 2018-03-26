b079c4d2c0faf64fabc20c462500e4c814ea2547
cindyloppar <cindyloppar@gmail.com>
2018-03-26 12:23:40 +0200

    I changed the name to input in the state

--- code-pen-projects/markdown/src/Layout.js
+++ code-pen-projects/markdown/src/Layout.js
@@ -1,40 +1,36 @@
 
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import ReactMarkdown from 'react-markdown';
 
 
- const input = '# This is a header\n\nAnd this is a paragraph'
 
 export default class Layout extends React.Component {
     constructor() {
         super();
         this.state = {
-            name: ""
+            input: ""
         }
     }
 
     changeDescription(newValue) {
-        this.setState({ name: newValue });
+        this.setState({ input: newValue });
     }
     handleChange(e) {
         const description = e.target.value;
         this.changeDescription(description);
     }
 
-    
-
     render() {
-        <ReactMarkdown source={input} /> ,
+        <ReactMarkdown source={this.state.input} /> ,
             document.getElementById('container')
         return (
             <div>
                 <h1> Markdown Previewer</h1>
                 <input onChange={this.handleChange.bind(this)} />
-                <p>{this.state.name} </p>
+                <p>{this.state.input} </p>
             </div>
 
         );
     }
 }
-
