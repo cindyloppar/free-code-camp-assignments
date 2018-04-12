efe9d0663271845a544c8d544531326a5ad84da1
cindyloppar <cindyloppar@gmail.com>
2018-04-12 14:41:14 +0200

    I changed the state to be in object and added functions to display after adding a recipe

--- code-pen-projects/recipe/src/View.js
+++ code-pen-projects/recipe/src/View.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import Button from "react-bootstrap/lib/Button";
 import Modal from "react-bootstrap/lib/Modal";
-import Recipe from './Recipe';
+// import Recipe from './Recipe';
 
 export default class View extends React.Component {
     constructor() {
         super();
         this.state = {
-            show: false
+            show: false,
+            recipe: [{ recipeName: "cake", ingredients: ["milk", "baking powder", 'cocoa', 'Rama'] }],
+            ingredients: '',
+            recipeName: '',
         }
         this.handleClose = this.handleClose.bind(this);
         this.handleShow = this.handleShow.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeIngredients = this.handleChangeIngredients.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ recipeName: e.target.value });
+        console.log(this.state.recipeName);
+    }
+
+    handleChangeIngredients(e) {
+
+        this.setState({ ingredients: e.target.value.split(',') });
+        console.log("ingredients", this.state.ingredients)
     }
 
     handleClose() {
@@ -21,6 +37,12 @@ export default class View extends React.Component {
         this.setState({ show: true });
     }
 
+    handleSaveChanges() {
+        var newItem = { recipeName: this.state.recipeName, ingredients: this.state.ingredients };
+        this.setState({ recipe: this.state.recipe.push(newItem) });
+        console.log(this.state)
+    }
+
     render() {
         return (
             <div className="static-modal">
@@ -29,7 +51,7 @@ export default class View extends React.Component {
                     bsStyle="primary"
                     bsSize="large"
                     onClick={() => this.setState({ show: true })}
-                    
+
                 >
                     Add Recipe
                 </Button>
@@ -39,23 +61,32 @@ export default class View extends React.Component {
                     container={this}
                     aria-labelledby="contained-modal-title"
                 >
-
                     <Modal.Header>
                         <Modal.Title>
-                            <input placeholder="dish name" />
+                            <input placeholder="dish name" onChange={this.handleChange.bind(this)} />
                         </Modal.Title>
                     </Modal.Header>
 
                     <Modal.Body>
-                        <textarea placeholder="ingredients" />
+                        <textarea placeholder="ingredients" onChange={this.handleChangeIngredients.bind(this)} />
                     </Modal.Body>
 
                     <Modal.Footer>
-                        <Button onClick={this.handleClose}>Close</Button>
-                        <Button bsStyle="primary" onClick={this.handleClose}>Save changes</Button>
+                        <Button onClick={this.handleClose}>Edit</Button>
+                        <Button bsStyle="primary" onClick={this.handleSaveChanges.bind(this)}>Save changes</Button>
                     </Modal.Footer>
                 </Modal>
+                <div>
+                    {
+                        this.state.recipe.map(element => {
+                            return <div> <button>{element.recipeName}</button>
+                              <ul>{element.ingredients.map(singleItem => { return <ul>{singleItem}</ul> })}</ul>
+                            </div>
+                        })
+                    }
+                </div>
             </div>
+
         );
     }
 }
--- code-pen-projects/recipe/src/index.js
+++ code-pen-projects/recipe/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import View from './View';
-import Recipe from './Recipe'
+// import Recipe from './Recipe'
 import './index.css';
 
 const recipe = document.getElementById("root")
