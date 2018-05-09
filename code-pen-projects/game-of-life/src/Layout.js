e08a7273ee8e770bd1b14aa60445eac6baf6b72f
cindyloppar <cindyloppar@gmail.com>
2018-05-09 17:40:08 +0200

    display grid

--- code-pen-projects/game-of-life/src/Layout.js
+++ code-pen-projects/game-of-life/src/Layout.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { initialEmptyGrid, aliveCells, checkNeighbors } from "./use-javascript";
 
-export default class Layout extends React.Component{
-    constructor(){
+
+export default class Layout extends React.Component {
+    constructor() {
         super(),
-        this.state={
-            
+            this.state = {
+                grid: [],
 
-        }
+            }
     }
-        render(){
-            return(
-                <div>
-                    </div>
-            );
-        
+    componentDidMount() {
+        this.setState({ grid: initialEmptyGrid() });
+    }
+    render() {
+        return (
+            <div className="grid">
+                {this.state.grid.map(element => {
+                    return <button >{element.status}</button>
+                })}
+            </div>
+        );
+
     }
 }
--- code-pen-projects/game-of-life/src/use-javascript.js
+++ code-pen-projects/game-of-life/src/use-javascript.js
@@ -1,58 +1,77 @@
-var grid = [];
-function initialEmptyGrid(){
- 
-    for(var x = 0; x < 2; x++){
-        for(var y = 0; y < 2; y++){
+
+function initialEmptyGrid() {
+    var grid = [];
+    for (var x = 0; x < 5; x++) {
+        for (var y = 0; y < 5; y++) {
             grid.push({
-                x:x,
-                y:y,
-            status: "dead",
+                x: x,
+                y: y,
+                status: "dead",
             })
         }
     }
     return grid;
 }
-console.log(initialEmptyGrid());
+function aliveCells(impilo,grid) {
+    var initialAliveCells = [];
+    var aliveDeadGrid = grid;
+    if (!impilo) {
+        initialAliveCells = [{ x: 0, y: 0, status: "alive" }, { x: 0, y: 1, status: "alive" }, { x: 1, y: 0, status: "alive" }];
+
+    } else {
+        initialAliveCells = impilo;
+    }
+    for (var i = 0; i < aliveDeadGrid.length; i++) {
+        var cellFound = aliveDeadGrid.find(element => element.x === aliveDeadGrid[i].x && element.y === aliveDeadGrid[i].y);
+        aliveDeadGrid[aliveDeadGrid.indexOf(cellFound)].status = "alive";
+    }
+    return aliveDeadGrid
+}
+function checkForAliveNeighbors(obj) {
+    var neighbors = {};
+    var allNeighbors = [
+        { x: obj.x, y: obj.y + 1 },
+        { x: obj.x + 1, y: obj.y + 1 },
+        { x: obj.x + 1, y: obj.y },
+        { x: obj.x + 1, y: obj.y - 1 },
+        { x: obj.x, y: obj.y - 1 },
+        { x: obj.x - 1, y: obj.y - 1 },
+        { x: obj.x - 1, y: obj.y },
+        { x: obj.x - 1, y: obj.y + 1 }
+    ]
+    neighbors = { coordinate: obj, listOfNeighbors: allNeighbors };
+    return neighbors;
+}
 
 
-var initialGrid = initialEmptyGrid();
-function aliveCells(){
-    initialAliveCells = [{x:1, y:0,status:"alive"}, {x:0, y:1, status:"alive"}, {x:2, y:0, status: "alive"}];
-    for(var i = 0; i < initialGrid.length; i++){
-        for(var c = 0; c < initialAliveCells.length; c++){
-            if(initialGrid[i].x === initialAliveCells[c].x && initialGrid[i].y === initialAliveCells[c].y){
-                initialGrid[i] = initialAliveCells[c];
-    
+function checkNeighbors(displayGrid) {
+    var grid = displayGrid;
+    grid.forEach(element => {
+        var cellsNeighbor = checkForAliveNeighbors(element);
+        var aliveNeighbors = [];
+        var deadNeighbors = [];
+        cellsNeighbor.listOfNeighbors.forEach(item => {
+            var actualCell = grid.find(cell => cell.x === item.x && cell.y === item.y);
+            if (actualCell !== undefined && actualCell.status === "alive") {
+                aliveNeighbors.push(actualCell)
+            } else if (actualCell !== undefined && actualCell.status === "dead") {
+                deadNeighbors.push(actualCell)
             }
+        })
+        if (element.status === "dead" && deadNeighbors.length === 3) {
+            element.status = "alive";
+
+        } else if (element.status === "alive" && aliveNeighbors.length > 3) {
+            element.status = "dead"
+        } else if (element.status === "alive" && aliveNeighbors.length < 2) {
+            element.status = "dead";
+        } else if (element.status === "alive" && aliveNeighbors.length === 2 || aliveNeighbors.length === 3) {
+            element.status = "alive"
         }
-    }
-    return  initialGrid
+
+    });
+    return grid;
 }
 
- console.log(aliveCells());
 
-
-function checkForNeighbors(x,y){
-    var neighbors = [];
-for(var i = 0; i < initialGrid.length; i++){
-    var neighborToEval = [
-        {x:initialGrid[i].x, y:initialGrid[i].y+1},
-        {x:initialGrid[i].x+1, y:initialGrid[i].y+1},
-        {x:initialGrid[i].x+1, y:initialGrid[i].y},
-        {x:initialGrid[i].x+1, y:initialGrid[i].y-1},
-        {x:initialGrid[i].x, y:initialGrid[i].y-1},
-        {x:initialGrid[i].x-1, y:initialGrid[i].y-1},
-        {x:initialGrid[i].x-1, y:initialGrid[i].y},
-        {x:initialGrid[i].x-1, y:initialGrid[i].y-1}
-       ]
-        neighbors.push({coordinate:initialGrid[i],neighbors: neighborToEval})
-     }
-     return neighbors;
-    }
-console.log(checkForNeighbors())
-
-function  checkForAliveDeadCells(){
-    for(var i = 0; i < initialGrid.length; i++){
-        if( initialGrid[i] === "kckv" )
-    }
-}
+module.exports = { initialEmptyGrid, aliveCells, checkNeighbors }
