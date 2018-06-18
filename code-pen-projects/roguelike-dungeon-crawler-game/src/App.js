098940ab5b7bb597cd1fbe9f07b5a43828be2618
cindyloppar <cindyloppar@gmail.com>
2018-06-18 17:37:09 +0200

    added enemies on the grid

--- code-pen-projects/roguelike-dungeon-crawler-game/src/App.js
+++ code-pen-projects/roguelike-dungeon-crawler-game/src/App.js
@@ -8,26 +8,28 @@ class App extends Component {
       grid: [],
       status: '',
       player: { x: 1, y: 1, player: "", status: "pass" },
+      playerLife: 50,
       path: [],
-      health: 20,
-      weapon: 1,
+      health: [],
+      weapon: [],
       level: 1,
       enemies: [],
+      enemyLife: 20,
     }
     this.grid = this.grid.bind(this);
     this.pathWays = this.pathWays.bind(this);
-    this.creatingRandomEnemies = this.creatingRandomEnemies.bind(this)
+    this.creatingRandomEnemies = this.creatingRandomEnemies.bind(this);
+    this.playerMovesInTheGrid = this.playerMovesInTheGrid.bind(this);
   }
 
   componentDidMount() {
+    document.onkeydown = this.playerMovesInTheGrid;
     var allDeadCells = this.grid();
     var aliveAndDead = this.pathWays(allDeadCells);
 
-    this.creatingRandomEnemies(aliveAndDead.path)
-    this.setState({ grid: aliveAndDead.grid, path: aliveAndDead.path })
-    // this.setState({ grid:this.grid()})
+    var newGrid = this.creatingRandomEnemies(aliveAndDead.grid)
+    this.setState({ grid: aliveAndDead.grid });
   }
-
 
   grid() {
     var deadCells = [];
@@ -46,22 +48,22 @@ class App extends Component {
   pathWays(deadGrid) {
     var cells = deadGrid;
     var paths = [
-      { x: 1, y: 1, player: "", status: "pass" },
-      { x: 2, y: 1, player: "", status: "pass" },
-      { x: 3, y: 1, player: "", status: "pass" },
-      { x: 4, y: 1, player: "", status: "pass" },
-      { x: 5, y: 1, player: "", status: "pass" },
-      { x: 6, y: 2, player: "", status: "pass" },
-      { x: 7, y: 2, player: "", status: "pass" },
-      { x: 8, y: 3, player: "", status: "pass" },
-      { x: 5, y: 3, player: "", status: "pass" },
-      { x: 32, y: 1, player: "", status: "pass" },
-      { x: 67, y: 3, player: "", status: "pass" },
-      { x: 18, y: 5, player: "", status: "pass" },
-      { x: 97, y: 2, player: "", status: "pass" },
-      { x: 64, y: 4, player: "", status: "pass" },
-      { x: 55, y: 2, player: "", status: "pass" },
-      { x: 87, y: 3, player: "", status: "pass" },
+      { x: 1, y: 1, player: ":)", status: "pass" },
+      { x: 0, y: 1, player: "", status: "pass" },
+      { x: 0, y: 2, player: "", status: "pass" },
+      { x: 1, y: 2, player: "", status: "pass" },
+      { x: 2, y: 2, player: "", status: "pass" },
+      { x: 3, y: 2, player: "", status: "pass" },
+      { x: 3, y: 3, player: "", status: "pass" },
+      { x: 4, y: 5, player: "", status: "/" },
+      { x: 3, y: 4, player: "", status: "pass" },
+      { x: 3, y: 5, player: "", status: "pass" },
+      { x: 5, y: 5, player: "", status: "pass" },
+      { x: 5, y: 6, player: "", status: "!" },
+      { x: 5, y: 7, player: "", status: "pass" },
+      { x: 5, y: 8, player: "", status: "pass" },
+      { x: 6, y: 7, player: "", status: "pass" },
+
     ];
     for (var i = 0; i < paths.length; i++) {
       var cellFound = cells.find(element => element.x === paths[i].x && element.y === paths[i].y);
@@ -69,67 +71,95 @@ class App extends Component {
         cells[cells.indexOf(cellFound)].status = "pass";
       }
     }
-    return { grid: cells, path: paths }
-
+    return { grid: cells, path: paths, player: paths }
   }
 
   creatingRandomEnemies(aliveCells) {
-    var onlyAlive = aliveCells;
+    var onlyAlive = aliveCells
     var randomArray = [];
-    while (randomArray.length < 5) {
+    var i = 5;
+    while (randomArray.length < i) {
       var randomNumber = Math.floor(Math.random() * onlyAlive.length);
-      var randomCell = { ...onlyAlive[randomNumber], status: "enemy" };
-      if (randomArray.indexOf(randomCell) === -1) {
+      var randomCell = { ...onlyAlive[randomNumber] };
+      if (randomCell.status === 'pass') {
+        randomCell.status = 'enemy'
         randomArray.push(randomCell);
       }
     }
+
     var gridWithoutEnemies = this.state.grid;
-    for (let index = 0; index < randomArray.length; index++) {
+    console.log("e", gridWithoutEnemies);
+    for (var index = 0; index < randomArray.length; index++) {
       var findMatch = onlyAlive.find(element => element.x === randomArray[index].x && element.y === randomArray[index].y);
-      if(onlyAlive[onlyAlive.indexOf(findMatch)]){
+      if (onlyAlive[onlyAlive.indexOf(findMatch)]) {
         onlyAlive[onlyAlive.indexOf(findMatch)].status = 'enemy';
-        console.log("randomArray", randomArray);
       }
     }
-    return {grid:randomArray, enemies:randomArray};
-    // for (var i = 0; i < randomArray.length; i++) {
-    //   var findMatch = randomCells.find(element => element.x === randomArray[i].x && element.y === randomArray[i].y);
-    //   console.log('path', findMatch);
-    //   if (randomCells[randomCells.indexOf(findMatch)]) {
-    //     randomCells[randomCells.indexOf(findMatch)].status = 'pass';
-    //   }
 
-    // }
-    // return { grid: randomCells, enemies: randomArray }
+    return { grid: randomArray, enemies: randomArray };
+
   }
-  // Random Position Generator
 
-  //  genPos = () =>{
-  //   return Number(Math.floor(Math.random()*30));
+  // placePlayerOnTheGrid(life) {
+  //   var alive = life;
+  //   var playerRandom = [];
+  //   while (playerRandom.length < 6) {
+  //     var randomPlacement = Math.floor(Math.random() * alive.length);
+  //     var copyFromRandomPlacement = { ...alive[randomPlacement] };
+  //     if (copyFromRandomPlacement.status === 'pass') {
+  //       copyFromRandomPlacement.status = "Play";
+  //       playerRandom.push(copyFromRandomPlacement);
+  //     }
   //   }
 
-  //  genAttack = (dungeon)=>{
-  //   return Number((dungeon)*Math.floor(Math.random()*10)+1)
+  //   // var gridWithDeadAliveAndEnemies = this.state.grid;
+  //   for (var c = 0; c < playerRandom.length; c++) {
+  //     var lookForMath = alive.find(element => element.x === playerRandom[c].x && element.y === playerRandom[c].y);
+  //     if (alive[alive.indexOf(lookForMath)]) {
+  //       alive[alive.indexOf(lookForMath)].status = 'play';
+  //       console.log(playerRandom, alive)
+  //     }
+
+  //   }
+  //   return playerRandom;
   // }
 
+
+  playerMovesInTheGrid(event) {
+    var keys = this.state.player;
+    if (event.key === "ArrowUp") {
+      keys = { x: keys.x + 1, y: keys.y }
+    } else if (event.key === "ArrowDown") {
+      keys = { x: keys.x - 1, y: keys.y }
+    } else if (event.key === "ArrowRight") {
+      keys = { x: keys.x, y: keys.y + 1 }
+    } else if (event.key === "ArrowLeft") {
+      keys = { x: keys.x, y: keys.y - 1 }
+    }
+    this.setState({ player: keys })
+  }
 
   render() {
     return (
       <div >
         <div className='heading'>
-          <h3>Dungeon Crawler Game</h3>
+          <h1>Roguelike Dungeon Crawler Game</h1>
         </div>
-        <div className="icons">
-          <ul className="fa-ul">
-            <li><span className="fa-li"><i className="fas fa-check-square"></i></span>List icons can</li>
-            <li><span className="fa-li"><i className="fas fa-check-square"></i></span>be used to</li>
-            <li><span className="fa-li"><i className="fas fa-spinner fa-pulse"></i></span>replace bullets</li>
-            <li><span className="fa-li"><i className="far fa-square"></i></span>in lists</li>
-          </ul>
+        <div className="message">
+          <span className='playerBlock'></span>
+          <span className='value'>Play</span>
+          <span className='healthBlock'></span>
+          <span className='value'>Health</span>
+          <span className='weaponBlock'></span>
+          <span className='value'>Weapon</span>
+          {/* <span className= 'foodBlock'></span>
+          <span className= 'value'>Food</span> */}
+          <span className='enemy'></span>
+          <span className='value'>Enemies</span>
         </div>
         <div className="grid">
           {this.state.grid.map(element => {
-            return <button onClick={() => this.grid()(element)} id={element.status}>{element.status}</button>
+            return <button onClick={() => this.grid()(element)} className={element.status}></button>
           })}
         </div>
       </div>
